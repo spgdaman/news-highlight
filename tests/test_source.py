7931522d8f1eee26e2f95b467c0a415a9f906f62
simon <gatheru.simon432@gmail.com>
2019-04-18 09:23:51 +0300

    Add license file

---
+++ tests/test_source.py
@@ -0,0 +1,9 @@
+from app import models
+import unittests
+
+class TestSource(TestCases):
+    '''
+    This class tests the behaviour of the source class
+    '''
+    models.Source()
+    self.assert()
