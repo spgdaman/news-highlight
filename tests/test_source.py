88e88507525e55ec13d4df516dc2cffd7d5fc71d
simon <gatheru.simon432@gmail.com>
2019-04-18 09:41:09 +0300

    test case passed

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,9 +1,26 @@
 from app import models
-import unittests
+import unittest
+Source = models.Source
 
-class TestSource(TestCases):
+class TestSource(unittest.TestCase):
     '''
     This class tests the behaviour of the source class
     '''
-    models.Source()
-    self.assert()
+
+    def setUp(self):
+        '''
+        Function that creates a new news object
+        '''
+        self.new_news = Source("123","CNN","The best news","https://www.cnn.com","politics","en","Kenya")
+
+    def test_init(self):
+        '''
+        Function that tests if the object is initialized correctly
+        '''
+        self.assertTrue(self.new_news.id,"123")
+        self.assertTrue(self.new_news.name,"CNN")
+        self.assertTrue(self.new_news.description,"The best news")
+        self.assertTrue(self.new_news.url,"https://www.cnn.com")
+        self.assertTrue(self.new_news.category,"politics")
+        self.assertTrue(self.new_news.language,"en")
+        self.assertTrue(self.new_news.country,"Kenya")
